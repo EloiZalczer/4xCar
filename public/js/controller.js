a44dccf192744e4a65653f427f688f05897a33ea
ezalczer <eloi.zalczer@polytech-lille.net>
2019-02-08 11:37:18 +0100

    Add support for mobile devices

--- public/js/controller.js
+++ public/js/controller.js
@@ -41,6 +41,35 @@ window.onload = function(){
 			}
 		}
 	}
+
+	// Support for mobile devices
+
+	canvas.addEventListener('touchstart', function(evt){
+		evt.preventDefault()
+		mousedown = true;
+	})
+
+	document.body.addEventListener('touchend', function(evt){
+		evt.preventDefault()
+		mousedown = false;
+		resetBall();
+	})
+
+	canvas.addEventListener('touchmove', function(evt){
+		if(mousedown){
+			var touches = evt.changedTouches;
+			coord = getTouchPos(evt)
+			console.log(coord)
+			prev_direction = command.direction;
+			prev_speed = command.speed;
+			command.direction = - Math.round(coord.x*(60/width) - 30);
+			command.speed = Math.round(30 - coord.y*(30/height));
+			displayBall(coord);
+			if(running && (prev_direction != command.direction || prev_speed != command.speed)){
+				sendData();
+			}
+		}
+	})
 }
 
 function connect(){
@@ -161,12 +190,26 @@ function displayBall(coord){
 }
 
 function  getMousePos(evt) {
-  var rect = canvas.getBoundingClientRect(), // abs. size of element
-      scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
-      scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+	var rect = canvas.getBoundingClientRect(), // abs. size of element
+	  scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+	  scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
 
-  return {
-    x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-    y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
-  }
+	return {
+	x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+	y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+	}
+}
+
+function getTouchPos(evt){
+
+	touches = evt.changedTouches
+
+	var rect = canvas.getBoundingClientRect(), // abs. size of element
+	  scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+	  scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+
+	  return {
+	    x: (touches[0].pageX - rect.left) * scaleX,   // scale mouse coordinates after they have
+	    y: (touches[0].pageY - rect.top) * scaleY     // been adjusted to be relative to element
+	  }
 }
